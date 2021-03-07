f38f2ba3e8c81055318ee79d6150f0b4bb16f01f
Ehsan QarehAgahji <80197312+EhsanQA@users.noreply.github.com>
2021-03-07 20:15:40 +0330

    Add files via upload

---
+++ Bank/src/sample/Client.java
@@ -0,0 +1,18 @@
+package sample;
+
+public class Client {
+    private long num;
+    private String name;
+    private String familyName;
+    private long balance;
+    private long password;
+    public Client(long num, String name, String familyName, long balance, long password) {
+        this.num = num;
+        this.name = name;
+        this.familyName = familyName;
+        this.balance = balance;
+        this.password = password;
+    }
+
+
+}
